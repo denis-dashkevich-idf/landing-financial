01661987b58d848b0828abbea4a1ab616bc0fdf3
denis-dashkevich-idf <denis.dashkevich@moneyman.ru>
2021-04-07 23:29:31 +0300

    add slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
+// header
 const burgerEl = document.querySelector('.header__burger');
 const menuWrapEl = document.querySelector('.header__menu-wrap');
 
 burgerEl.addEventListener('click', function(event) {
   menuWrapEl.classList.toggle('header__menu-wrap_visible');
+});
+
+// slider
+var swiper = new Swiper('.reviews__slider', {
+  loop: true,
 });
