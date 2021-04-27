4eb9a24528198c3d844ee23994347536eb9a1cf6
denis-dashkevich-idf <denis.dashkevich@moneyman.ru>
2021-04-27 03:08:21 +0300

    pre last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,12 @@ contactsFormEl.addEventListener('submit', function(event) {
   }
 
 });
+
+// theme switch
+const switchThemeEl = document.querySelector('.switch input');
+const htmlEl = document.querySelector('html');
+
+switchThemeEl.addEventListener('click', function(event) {
+  event.stopPropagation;
+  htmlEl.dataset.mode === 'light' ? htmlEl.dataset.mode = '' : htmlEl.dataset.mode = 'light';
+})
